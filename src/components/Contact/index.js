76298015c490cfd9b840ef07a54ae74099939eee
dan <danielornelas01@yahoo.com>
2022-10-14 11:30:24 -0700

    20.4.6

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,46 +1,79 @@
 import react, { useState } from "react";
-
-
-
+import { validateEmail } from "../../utils/helpers";
 
 function ContactForm() {
-    const [formState, setFormState] = useState({ name: '', email: '', message: '' });
-
-    const { name, email, message } = formState;
-
-
-    function handleChange(e) {
-        setFormState({...formState, [e.target.name]: e.target.value })
-    }
+  const [formState, setFormState] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+  const [errorMessage, setErrorMessage] = useState("");
+  const { name, email, message } = formState;
  
 
-    function handleSubmit(e) {
-        e.preventDefault();
-        console.log(formState);
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!errorMessage) {
+        setFormState({ ...formState, [e.target.name]: e.target.value });
+      }
+  }
+
+  function handleChange(e) {
+    if (e.target.name === 'email') {
+      const isValid = validateEmail(e.target.value);
+      console.log(isValid);
+      // isValid conditional statement
+      if (!isValid) {
+        setErrorMessage("Your email is invalid.");
+      } else {
+        setErrorMessage("");
+      }
+    } else {
+      if (!e.target.value.length) {
+        setErrorMessage(`${e.target.name} is required.`);
+      } else {
+        setErrorMessage("");
+      }
     }
 
-return (
+    console.log("errorMessage", errorMessage);
+  }
+
+  return (
     <section>
-        <h1>Contact Me</h1>
-        <form id="contact-form" onSubmit={handleSubmit}>
-            <div>
-                <label htmlFor="name">Name:</label>
-                <input type="text" defaultValue={name} onChange={handleChange} name="name" />
-            </div>
-            <div>
-                <label htmlFor="email">Email Address</label>
-                <input type="email" defaultValue={email} name="email" onChange={handleChange} />
-            </div>
-            <div>
-                <label htmlFor="message">Message</label>
-                <textarea name="message" defaultValue={message} onChange={handleChange} rows="5"  />
-            </div>
-            <button type="submit">Submit</button>
-        </form>
+      <h1>Contact Me</h1>
+      <form id="contact-form" onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="name">Name:</label>
+          <input
+            type="text"
+            defaultValue={name}
+            onChange={handleChange}
+            name="name"
+          />
+        </div>
+        <div>
+          <label htmlFor="email">Email Address</label>
+          <input
+            type="email"
+            defaultValue={email}
+            name="email"
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="message">Message</label>
+          <textarea
+            name="message"
+            defaultValue={message}
+            onChange={handleChange}
+            rows="5"
+          />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
     </section>
-);
+  );
 }
-
-
 
 export default ContactForm;
